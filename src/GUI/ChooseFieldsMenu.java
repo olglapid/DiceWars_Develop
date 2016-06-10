4a30b22ba475bb8ae98d644348389ce0edaf5907
Ola <olglapid@htwg-konstanz.de>
2016-06-10 13:06:27 +0200

    gui menu

---
+++ src/GUI/ChooseFieldsMenu.java
@@ -0,0 +1,43 @@
+package GUI;
+
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.border.*;
+
+@SuppressWarnings("serial")
+public class ChooseFieldsMenu extends JPanel implements ActionListener {
+
+	private JTextField fieldsEingabe;
+	private JButton anwenden;
+	
+	public ChooseFieldsMenu() {
+		
+		JPanel panel = new JPanel();
+		panel.setLayout(new GridLayout(0, 1));
+		panel.add(new JLabel("Felderanzahl eingeben (max 300)"));
+		
+		JPanel panel1 = new JPanel();
+		panel1.setLayout(new GridLayout(0, 1));
+		fieldsEingabe = new JTextField("", 3);
+		panel1.add(fieldsEingabe);
+		
+		Border border = BorderFactory.createTitledBorder("Felderanzahl Auswahl");
+		this.setBorder(border);
+		this.setLayout(new BoxLayout(this, BoxLayout.LINE_AXIS));
+		this.add(panel);
+		this.add(panel1);
+		anwenden = new JButton("Anwenden");
+		this.add(anwenden);
+		anwenden.addActionListener(this);
+		
+	}
+	
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+}
