b68d895a31aaaa94e88944fd38a66c7eb964ac23
Ola <olglapid@htwg-konstanz.de>
2016-06-23 17:02:08 +0200

    modified

--- src/GUI/ChooseFieldsPanel.java
+++ src/GUI/ChooseFieldsPanel.java
@@ -10,15 +10,17 @@ import javax.swing.border.*;
 public class ChooseFieldsPanel extends JPanel implements MouseListener {
 
 	private JTextField fieldsEingabe;
-	private JButton anwenden;
+	private JButton anwenden, start;
 	
 	public ChooseFieldsPanel() {
 		
 		JPanel panel = new JPanel();
 		panel.setLayout(new GridLayout(0, 1));
+		panel.setSize(250, 200);
 		panel.add(new JLabel("Felderanzahl eingeben (max 300)"));
 		
 		JPanel panel1 = new JPanel();
+		panel1.setSize( 0, 80);
 		panel1.setLayout(new GridLayout(0, 1));
 		fieldsEingabe = new JTextField("", 3);
 		panel1.add(fieldsEingabe);
@@ -29,8 +31,12 @@ public class ChooseFieldsPanel extends JPanel implements MouseListener {
 		this.add(panel);
 		this.add(panel1);
 		anwenden = new JButton("Anwenden");
-		this.add(anwenden);
+		panel1.add(anwenden);
 		anwenden.addMouseListener(this);
+		
+		start = new JButton("Start");
+		panel1.add(start);
+		start.addMouseListener(this);
 	}
 	
 
@@ -38,7 +44,30 @@ public class ChooseFieldsPanel extends JPanel implements MouseListener {
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		// TODO Auto-generated method stub
+		Object source = e.getSource();
+		if(source == anwenden) {
+			
+		}
 		
+		if(source == start) {
+			if(fieldsEingabe.getText().length() == 0) {
+				JOptionPane.showMessageDialog(
+					    fieldsEingabe, "Waehle Anzahl Felder und Anzahl Spieler!",
+					    "Eingabefehler",
+					    JOptionPane.ERROR_MESSAGE);
+			} else {
+				JFrame frame = new JFrame("Spielfeld");
+				frame.setSize(400, 400);
+				frame.setLayout(new GridLayout(1, 0));
+				
+				GameField game = new GameField();
+				
+				//frame.add(game);
+				frame.pack();
+				frame.setVisible(true);
+			}
+			
+		}
 	}
 
 
