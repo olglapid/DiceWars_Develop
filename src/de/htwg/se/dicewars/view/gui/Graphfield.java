98a60ddf0a3e2010986c56910a01b7288e0c3af7
Simon Christofzik <christofzik.simon@web.de>
2016-06-24 10:43:41 +0200

    Feld kann farben ändern

--- src/de/htwg/se/dicewars/view/gui/Graphfield.java
+++ src/de/htwg/se/dicewars/view/gui/Graphfield.java
@@ -1,6 +1,7 @@
 package de.htwg.se.dicewars.view.gui;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Polygon;
@@ -11,25 +12,60 @@ import java.awt.event.MouseEvent;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-
-
 public class Graphfield extends JPanel {
 	Polygon[][] poly;
+	int[][] field;
 	int size;
 	int radius;
 	Graphics g;
+	Color color;
+
 	public Graphfield(int length, int r) {
-		radius=r;
-		size=length;
-		poly=new Polygon[length][length];
+		radius = r;
+		color=Color.BLACK;
+		size = length;
+		poly = new Polygon[length][length];
+		field= new int[length][length];
+		for (int i = 0; i < field.length; i++) {
+			for (int j = 0; j < field.length; j++) {
+				field[i][j]=0;
+			}
+		}
+		field[length-1][length-1]=1;
 		MyMouseListener m1 = new MyMouseListener();
 		addMouseListener(m1);
-		//Hexdrawer.drawHex(g, length, r, poly);
+		// Hexdrawer.drawHex(g, length, r, poly);
 	}
 
 	public void paintComponent(Graphics g) {
-		Hexdrawer.drawHex(g, size, radius,poly);
-		this.g=g;
+		Graphics2D g2 = (Graphics2D) g;
+		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		g.setFont(new Font("TimesRoman", Font.PLAIN, 20));
+		super.paintComponent(g2);
+		int xcord = 0, ycord = 0;
+		for (int i = 0; i < size; i++) {
+			ycord += radius + radius - (2 * radius / hexdrawer.SIDES);
+			if (i % 2 != 0)
+				xcord += radius - (radius / (2 * hexdrawer.SIDES));
+			for (int j = 0; j < size; j++) {
+				xcord += radius + radius - (radius / hexdrawer.SIDES);
+				poly[i][j] = hexdrawer.drawhexagon(xcord, ycord, radius, g2, Color.PINK);
+			}
+			xcord = 0;
+		}
+		xcord=0;
+		ycord=0;
+		for (int i = 0; i < size; i++) {
+			ycord += radius + radius - (2 * radius / hexdrawer.SIDES);
+			if (i % 2 != 0)
+				xcord += radius - (radius / (2 * hexdrawer.SIDES));
+			for (int j = 0; j < size; j++) {
+				xcord += radius + radius - (radius / hexdrawer.SIDES);
+				hexdrawer.drawchange(xcord, ycord, g2, field[i][j], radius,color);
+			}
+			xcord = 0;
+		}
+
 	}
 
 	public static void main(String[] args) {
@@ -37,105 +73,76 @@ public class Graphfield extends JPanel {
 		JPanel p = new JPanel();
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		p.setSize(600, 800);
-		f.add(new Graphfield(12,50));
+		f.add(new Graphfield(12, 50));
 		// f.getContentPane().add(p);
 		f.setSize(800, 800);
 		f.setVisible(true);
 	}
-	
-	class MyMouseListener extends MouseAdapter	{
-		public void mouseClicked(MouseEvent e) { 
+
+	class MyMouseListener extends MouseAdapter {
+		public void mouseClicked(MouseEvent e) {
 			int x = e.getX();
 			int y = e.getY();
 			getField(x, y);
+			repaint();
 			
+
 		}
 	}
-	public int getField(int x, int y){
+
+	public int getField(int x, int y) {
 		for (int i = 0; i < poly.length; i++) {
 			for (int j = 0; j < poly.length; j++) {
-				if(poly[i][j].contains(x, y)){
+				if (poly[i][j].contains(x, y)) {
+					field[i][j]=(int)'X';
+					color=Color.GRAY ;
 					
-					System.out.println("Field "+(i*poly.length+(j+1)));
-					redraw(i, j);
-					return (i*poly.length+(j+1));
+					
+					System.out.println("Field " + (i * poly.length + (j + 1)));
+					return (i * poly.length + (j + 1));
 				}
 			}
 		}
 		return 0;
 	}
-	public void redraw(int x, int y){
-		Hexdrawer.repaint(g, poly.length, poly);
-		
-	
-	}
 
 }
-
-class Hexdrawer {
-	static final int SIDES = 6;
-
-	public static Polygon hexagon(int x, int y, int r, Color color) {
-		Polygon p = new Polygon();
-	
-		int[] xcord = new int[SIDES];
-		xcord[0]=x;
-		xcord[1]=x+r-(r/SIDES);
-		xcord[2]=x+r-(r/SIDES);
-		xcord[3]=x;
-		xcord[4]=x-r+(r/SIDES);
-		xcord[5]=x-r+(r/SIDES);
-		
-		int[] ycord = new int[SIDES];
-		ycord[0]=y+r;
-		ycord[1]=y+(r/2);
-		ycord[2]=y-(r/2);
-		ycord[3]=y-r;
-		ycord[4]=y-(r/2);
-		ycord[5]=y+(r/2);
-		
-		for (int i = 0; i < SIDES; i++) {
-			p.addPoint(xcord[i], ycord[i]);
-		}
-		return p;
-	}
-
-	public static void drawHex(Graphics g, int length, int r,Polygon[][] poly) {
-		Polygon p;
-		Graphics2D g2 = (Graphics2D)g;
-		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-		int xcord = 0, ycord = 0;
-		for (int x = 0; x < length; x++) {
-			ycord += r + r-(2*r/SIDES);
-			if (x % 2 != 0)
-				xcord +=r-(r/(2*SIDES));
-
-			for (int y = 0; y < length; y++) {
-
-				xcord += r + r-(r/SIDES);
-				System.out.println(xcord + " " + ycord);
-				p = hexagon(xcord, ycord, r, Color.BLUE);
-				poly[x][y]=p;
-				g2.setColor(Color.BLACK);
-				g2.drawPolygon(p);
-				g2.setColor(Color.WHITE);
-				g2.fillPolygon(p);
-			}
-			xcord = 0;
-		}
-	}
-	public static void repaint(Graphics g, int length,Polygon[][] poly){
-		Graphics2D g2 = (Graphics2D)g;
-		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-		for (int i = 0; i < poly.length; i++) {
-			for (int j = 0; j < poly.length; j++) {
-				g2.setColor(Color.GREEN);
-				g2.drawPolygon(poly[i][j]);
-				g2.setColor(Color.RED);
-				g2.fillPolygon(poly[i][j]);
-			}
-		}
-	}
-	
-
-}
+/*
+ * class Hexdrawer { static final int SIDES = 6;
+ * 
+ * public static Polygon hexagon(int x, int y, int r, Color color) { Polygon p =
+ * new Polygon();
+ * 
+ * int[] xcord = new int[SIDES]; xcord[0]=x; xcord[1]=x+r-(r/SIDES);
+ * xcord[2]=x+r-(r/SIDES); xcord[3]=x; xcord[4]=x-r+(r/SIDES);
+ * xcord[5]=x-r+(r/SIDES);
+ * 
+ * int[] ycord = new int[SIDES]; ycord[0]=y+r; ycord[1]=y+(r/2);
+ * ycord[2]=y-(r/2); ycord[3]=y-r; ycord[4]=y-(r/2); ycord[5]=y+(r/2);
+ * 
+ * for (int i = 0; i < SIDES; i++) { p.addPoint(xcord[i], ycord[i]); } return p;
+ * }
+ * 
+ * public static void drawHex(Graphics g, int length, int r,Polygon[][] poly) {
+ * Polygon p; Graphics2D g2 = (Graphics2D)g;
+ * g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+ * RenderingHints.VALUE_ANTIALIAS_ON); int xcord = 0, ycord = 0; for (int x = 0;
+ * x < length; x++) { ycord += r + r-(2*r/SIDES); if (x % 2 != 0) xcord
+ * +=r-(r/(2*SIDES));
+ * 
+ * for (int y = 0; y < length; y++) {
+ * 
+ * xcord += r + r-(r/SIDES); System.out.println(xcord + " " + ycord); p =
+ * hexagon(xcord, ycord, r, Color.BLUE); poly[x][y]=p; g2.setColor(Color.BLACK);
+ * g2.drawPolygon(p); g2.setColor(Color.WHITE); g2.fillPolygon(p); } xcord = 0;
+ * } } public static void repaint(Graphics g, int length,Polygon[][] poly){
+ * Graphics2D g2 = (Graphics2D)g;
+ * g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+ * RenderingHints.VALUE_ANTIALIAS_ON); for (int i = 0; i < poly.length; i++) {
+ * for (int j = 0; j < poly.length; j++) { g2.setColor(Color.GREEN);
+ * g2.drawPolygon(poly[i][j]); g2.setColor(Color.RED);
+ * g2.fillPolygon(poly[i][j]); } } }
+ * 
+ * 
+ * }
+ */
---
+++ src/de/htwg/se/dicewars/view/gui/hexdrawer.java
@@ -0,0 +1,95 @@
+package de.htwg.se.dicewars.view.gui;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Polygon;
+import java.awt.RenderingHints;
+
+public class hexdrawer {
+	static final int SIDES = 6;
+
+	public static Polygon hexagon(int x, int y, int r) {
+		Polygon p = new Polygon();
+
+		int[] xcord = new int[SIDES];
+		xcord[0] = x;
+		xcord[1] = x + r - (r / SIDES);
+		xcord[2] = x + r - (r / SIDES);
+		xcord[3] = x;
+		xcord[4] = x - r + (r / SIDES);
+		xcord[5] = x - r + (r / SIDES);
+
+		int[] ycord = new int[SIDES];
+		ycord[0] = y + r;
+		ycord[1] = y + (r / 2);
+		ycord[2] = y - (r / 2);
+		ycord[3] = y - r;
+		ycord[4] = y - (r / 2);
+		ycord[5] = y + (r / 2);
+
+		for (int i = 0; i < SIDES; i++) {
+			p.addPoint(xcord[i], ycord[i]);
+		}
+		return p;
+	}
+
+	public static Polygon drawhexagon(int x, int y, int r, Graphics2D g2, Color color) {
+		Polygon p = hexagon(x, y, r);
+		g2.setColor(color);
+		System.out.println("HIEER");
+		g2.fillPolygon(p);
+		g2.setColor(Color.WHITE);
+		g2.drawPolygon(p);
+		
+		return p;
+	}
+	
+	public static void drawchange(int i,int j,Graphics2D g2,int n,int r,Color color){
+		char c ='c';
+		//g2.setColor(color);
+		if(n>0){
+			g2.fillPolygon(hexagon(i, j, r));
+		g2.setColor(Color.GRAY);
+		c = (char) (n);
+		g2.drawPolygon(hexagon(i, j, r));
+		drawhexagon(i, j, r, g2, color);
+		g2.drawString(""+c, i, j);
+		}
+		else {
+			g2.setColor(Color.RED);
+			g2.fillPolygon(hexagon(i, j, r));
+			g2.setColor(Color.GRAY);
+			c = (char) (n);
+			g2.drawPolygon(hexagon(i, j, r));
+			drawhexagon(i, j, r, g2, Color.GREEN);
+			g2.drawString(""+c, i, j);
+		}
+		
+	}
+	/*public static void drawHex(Graphics g, int length, int r,Polygon[][] poly) {
+		Polygon p;
+		Graphics2D g2 = (Graphics2D)g;
+		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		int xcord = 0, ycord = 0;
+		for (int x = 0; x < length; x++) {
+			ycord += r + r-(2*r/SIDES);
+			if (x % 2 != 0)
+				xcord +=r-(r/(2*SIDES));
+
+			for (int y = 0; y < length; y++) {
+
+				xcord += r + r-(r/SIDES);
+				System.out.println(xcord + " " + ycord);
+				p = hexagon(xcord, ycord, r, Color.BLUE);
+				poly[x][y]=p;
+				g2.setColor(Color.BLACK);
+				g2.drawPolygon(p);
+				g2.setColor(Color.WHITE);
+				g2.fillPolygon(p);
+			}
+			xcord = 0;
+		}
+	}
+*/
+}
