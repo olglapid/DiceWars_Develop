d813331221ad179106f6654d4ed5b237f3be6011
Simon <christofzik.simon@web.de>
2016-06-30 02:22:45 +0200

    Technische Schulden abgebaut

---
+++ test/de/htwg/se/dicewars/state/MessageTest.java
@@ -0,0 +1,17 @@
+package de.htwg.se.dicewars.state;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+
+public class MessageTest {
+
+	@Test
+	public final void messageTest() {
+		StatusMessage text = null;
+		assertNotNull(text.text.values());
+	}
+
+}
